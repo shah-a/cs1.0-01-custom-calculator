0033893fc2a7ac40870dac218b92b05bcc85b6d7
Ali Shah <s.ashah@outlook.com>
2020-09-04 06:15:35 -0400

    basic functionality

---
+++ calc.py
@@ -0,0 +1,10 @@
+# Ali Shah
+# 2020-09-03
+# CS1.0 Assignment 1: Custom Calculator
+
+#This program calculates ...
+
+name = input()
+time = float(input())
+distance = float(input())
+speed = distance/time
