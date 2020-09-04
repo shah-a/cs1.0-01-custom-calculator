c9549b208efdc655cb74d29a2df9c2429331acb3
shah-a <38513271+shah-a@users.noreply.github.com>
2020-09-04 07:16:40 -0400

    updated comments

--- calc.py
+++ calc.py
@@ -2,8 +2,8 @@
 # 2020-09-03
 # CS1.0 Assignment 1: Custom Calculator
 
-#This program asks user to input time and distance, then calculates speed of travel
-#Calculator assumes constant values and an ideal physics world
+# This program asks user to input time and distance, then calculates approximate speed of travel
+# Calculator assumes ideal physics world and constant values
 
 # ASCII art by Daniel C. Au, taken from:
 # https://asciiart.website/index.php?art=objects/balloons
@@ -28,9 +28,9 @@ art = [
 "         |  `",
 "         '",
 "\n-------------------\n"
-]
+] #ascii art, indexed line-by-line in list for easy printing
 
-for _ in range(len(art)):
+for _ in range(len(art)): #for loop to print ascii art
     print(art[_])
 
 print(
@@ -38,10 +38,15 @@ print(
     "Are you stuck up there??\n"
 )
 
+# Next few lines assign username, time-of-flight, and distance travelled to "name", "time", and "distance" variables respectively
+# Note: time is multiplied by 60 to convert minutes to seconds (SI unit)
 name = input("What's your name?: ")
-time = float(input(f"How long have you been floating like that, {name}? (minutes): ")) * 60
-distance = float(input("How far have you travelled since you got stuck? (meters): "))
-speed = distance/time
+time = float(input(f"How long have you been floating like that, {name}? (minutes): ")) * 60 #str input cast as float
+distance = float(input("How far have you travelled since you got stuck? (meters): ")) #str input cast as float
+speed = distance/time #speed calculation formula; SI unit intended (m/s)
 
-print("\nThat means you've been travelling at " + "{:.2f}".format(speed) + "m/s!")
+#speed *= 3.600 #convert m/s to km/h; if used, change print statement label below
+#speed *= 2.237 #convert m/s mph; if used, change print statement label below
+
+print("\nThat means you've been travelling at " + "{:.2f}".format(speed) + "m/s!") #speed printed to 2 decimal places
 print("Let's get you down :)\n")
