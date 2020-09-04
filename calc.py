a9b2eb781b61c9b0050171c15a840b598116c0e7
Ali Shah <s.ashah@outlook.com>
2020-09-04 06:22:31 -0400

    made it pretty

--- calc.py
+++ calc.py
@@ -2,9 +2,46 @@
 # 2020-09-03
 # CS1.0 Assignment 1: Custom Calculator
 
-#This program calculates ...
+#This program asks user to input time and distance, then calculates speed of travel
+#Calculator assumes constant values and an ideal physics world
 
-name = input()
-time = float(input())
-distance = float(input())
+# ASCII art by Daniel C. Au, taken from:
+# https://asciiart.website/index.php?art=objects/balloons
+
+art = [
+"     ,;|%;Oo,",
+"   ,|||%%;;OOo,",
+"  ;||| CS10 OOo;",
+"  ;|||%%%;;;OOo;",
+"   '|||%%;;OOo'",
+"    \\'||%;OO'/",
+"     \\ |  | /",
+"      \\|__|/",
+"      |6:|6|",
+"      `;;|;'",
+"         |",
+"        ,'",
+"        ',",
+"          ;   help!",
+"          |o /",
+"       `\\ /\\",
+"         |  `",
+"         '",
+"\n-------------------\n"
+]
+
+for _ in range(len(art)):
+    print(art[_])
+
+print(
+    "Oh my! Is that a giant helium balloon?!\n"
+    "Are you stuck up there??\n"
+)
+
+name = input("What's your name?: ")
+time = float(input(f"How long have you been floating like that, {name}? (minutes): ")) * 60
+distance = float(input("How far have you travelled since you got stuck? (meters): "))
 speed = distance/time
+
+print("\nThat means you've been travelling at " + "{:.2f}".format(speed) + "m/s!")
+print("Let's get you down :)\n")
